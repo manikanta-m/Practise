762af82b169637a6e56c7cfe6fbb0d560b52f65d
IMIDOMAIN\manikanta.m <manikanta.m@imimobile.com>
2020-09-07 05:36:41 +0530

    added basic node of linked list

---
+++ src/main/java/com/problemsolving/practise/linkedlist/single/LinkedList.java
@@ -0,0 +1,13 @@
+package com.problemsolving.practise.linkedlist.single;
+
+public class LinkedList {
+
+	class Node {
+		int data;
+		Node nex;
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+}
